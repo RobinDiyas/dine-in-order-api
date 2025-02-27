43eb1277cf734eb5ec2c48667f4c4e5b3c9e5e9e
RobinDiyas <rdiyas78@gmail.com>
2025-02-27 17:54:14 +0530

    Adding Admin Staff and User

--- src/main/java/com/example/dio/model/Admin.java
+++ src/main/java/com/example/dio/model/Admin.java
@@ -1,4 +1,10 @@
 package com.example.dio.model;
 
-public class Admin {
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "admins")
+public class Admin extends User{
 }
--- src/main/java/com/example/dio/model/User.java
+++ src/main/java/com/example/dio/model/User.java
@@ -1,4 +1,45 @@
 package com.example.dio.model;
+import com.example.dio.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "users")
+@Inheritance(strategy = InheritanceType.JOINED)
+@Getter
+@Setter
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name = "userId")
+    private long userId;
+
+    @Column(name = "username")
+    private String  username;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "ph_no")
+    private String phoneNumber;
+
+    @Column(name = "role")
+    private UserRole userRole;
+
+    @Column(name = "craeted_at")
+    private LocalDate createdAt;
+
+    @Column(name = "last_modified_at")
+    private LocalDate lastModified;
+
+
+
+
 }
